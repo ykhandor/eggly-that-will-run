ac9e612705666055924498ffb8eea8e2bea1109d
ykhandor <yash.khandor1@gmail.com>
2015-02-18 23:47:59 -0800

    Assignment Changes

--- src/app/categories/bookmarks/bookmarks.js
+++ src/app/categories/bookmarks/bookmarks.js
@@ -1,43 +1,47 @@
 angular.module('bookmarks', [
-  'categories.bookmarks.edit',
-  'categories.bookmarks.create',
-  'eggly.models.categories',
-  'eggly.models.bookmarks'
+    'categories.bookmarks.edit',
+    'categories.bookmarks.create',
+    'eggly.models.categories',
+    'eggly.models.bookmarks'
 ])
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('eggly.categories.bookmarks', {
-        url: 'categories/:category',
-        views: {
-          'bookmarks@': {
-            controller: 'BookmarksCtrl',
-            templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html'
-          }
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('eggly.categories.bookmarks', {
+                url: 'categories/:category',
+                views: {
+                    'bookmarks@': {
+                        controller: 'BookmarksCtrl',
+                        templateUrl: 'app/categories/bookmarks/bookmarks.tmpl.html'
+                    }
+                }
+            })
+        ;
+    })
+    .controller('BookmarksCtrl', function BookmarksCtrl($scope, $stateParams, bookmarks, categories) {
+
+        categories.setCurrentCategory();
+
+        if ($stateParams.category) {
+            categories.getCategoryByName($stateParams.category).then(function (category) {
+                categories.setCurrentCategory(category);
+            })
         }
-      })
-    ;
-  })
-  .controller('BookmarksCtrl', function BookmarksCtrl($scope, $stateParams, bookmarks, categories) {
-    categories.setCurrentCategory();
 
-    if ($stateParams.category) {
-      categories.getCategoryByName($stateParams.category).then(function (category) {
-        categories.setCurrentCategory(category);
-      })
-    }
+        bookmarks.getBookmarks()
+            .then(function (result) {
+                $scope.bookmarks = result;
+            });
 
-    bookmarks.getBookmarks()
-      .then(function (result) {
-        $scope.bookmarks = result;
-      });
+        // $scope.getCurrentCategory = categories.getCurrentCategory;
+        $scope.getCurrentCategory = categories.execute("getCurrentCategory");
 
-    $scope.getCurrentCategory = categories.getCurrentCategory;
-    $scope.getCurrentCategoryName = categories.getCurrentCategoryName;
-    $scope.isSelectedBookmark = function (bookmarkId) {
-      return $stateParams.bookmarkId == bookmarkId;
-    };
+        //$scope.getCurrentCategoryName = categories.getCurrentCategoryName;
+        $scope.getCurrentCategoryName = categories.execute("getCurrentCategoryName");
 
-    $scope.deleteBookmark = bookmarks.deleteBookmark;
-  })
+        $scope.isSelectedBookmark = function (bookmarkId) {
+            return $stateParams.bookmarkId == bookmarkId;
+        };
+
+        $scope.deleteBookmark = bookmarks.deleteBookmark;
+    })
 ;
-
--- src/app/common/models/categories-model.js
+++ src/app/common/models/categories-model.js
@@ -1,70 +1,74 @@
 angular.module('eggly.models.categories', [
 
 ])
-  .service('categories', function CategoriesService($http, $q) {
-    var URLS = {
-        FETCH: 'data/categories.json'
-      },
-      categories,
-      currentCategory,
-      categoriesModel = this;
+    .service('categories', function CategoriesService($http, $q) {
+        var URLS = {
+                FETCH: 'data/categories.json'
+            },
+            categories,
+            currentCategory,
+            categoriesModel = this;
 
-    function extract(result) {
-      return result.data;
-    }
+        function extract(result) {
+            return result.data;
+        }
 
-    function cacheCategories(result) {
-      categories = extract(result);
-      return categories;
-    }
+        function cacheCategories(result) {
+            categories = extract(result);
+            return categories;
+        }
 
-    categoriesModel.getCategories = function () {
-      return (categories) ? $q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
-    };
+        categoriesModel.execute = function(functionName) {
+            return categoriesModel[functionName];
+        };
 
-    categoriesModel.getCurrentCategory = function () {
-      return currentCategory;
-    };
+        categoriesModel.getCategories = function () {
+            return (categories) ? $q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
+        };
 
-    categoriesModel.getCurrentCategoryName = function () {
-      return currentCategory ? currentCategory.name : '';
-    };
+        categoriesModel.getCurrentCategory = function () {
+            return currentCategory;
+        };
 
-    categoriesModel.setCurrentCategory = function (category) {
-      currentCategory = category;
-      return currentCategory;
-    };
+        categoriesModel.getCurrentCategoryName = function () {
+            return currentCategory ? currentCategory.name : '';
+        };
 
-    categoriesModel.createCategory = function (category) {
-      category.id = categories.length;
-      categories.push(category);
-    };
+        categoriesModel.setCurrentCategory = function (category) {
+            currentCategory = category;
+            return currentCategory;
+        };
 
-    categoriesModel.deleteCategory = function (category) {
-      _.remove(categories, function (c) {
-        return c.id == category.id;
-      });
-    };
+        categoriesModel.createCategory = function (category) {
+            category.id = categories.length;
+            categories.push(category);
+        };
 
-    categoriesModel.getCategoryByName = function (categoryName) {
-      var deferred = $q.defer();
+        categoriesModel.deleteCategory = function (category) {
+            _.remove(categories, function (c) {
+                return c.id == category.id;
+            });
+        };
 
-      function findCategory() {
-        return _.find(categories, function (c) {
-          return c.name == categoryName;
-        })
-      }
+        categoriesModel.getCategoryByName = function (categoryName) {
+            var deferred = $q.defer();
 
-      if (categories) {
-        deferred.resolve(findCategory());
-      } else {
-        categoriesModel.getCategories().then(function () {
-          deferred.resolve(findCategory());
-        })
-      }
+            function findCategory() {
+                return _.find(categories, function (c) {
+                    return c.name == categoryName;
+                })
+            }
 
-      return deferred.promise;
-    };
+            if (categories) {
+                deferred.resolve(findCategory());
+            } else {
+                categoriesModel.getCategories().then(function () {
+                    deferred.resolve(findCategory());
+                })
+            }
 
-  })
+            return deferred.promise;
+        };
+
+    })
 ;
